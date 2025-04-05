77ff88831f7ba8e2f2a5eeb68134d645ae165219
Luis Cataño <lcatano@Luiss-MacBook-Pro.local>
2025-04-05 17:06:19 -0500

    fixes

--- packages/admin/dashboard/src/routes/login/login.tsx
+++ packages/admin/dashboard/src/routes/login/login.tsx
@@ -6,7 +6,6 @@ import { Link, useLocation, useNavigate } from "react-router-dom"
 import * as z from "zod"
 
 import { Form } from "../../components/common/form"
-import AvatarBox from "../../components/common/logo-box/avatar-box"
 import { useSignInWithEmailPass } from "../../hooks/api"
 import { isFetchError } from "../../lib/is-fetch-error"
 import { useExtension } from "../../providers/extension-provider"
@@ -73,9 +72,8 @@ export const Login = () => {
   return (
     <div className="bg-ui-bg-subtle flex min-h-dvh w-dvw items-center justify-center">
       <div className="m-4 flex w-full max-w-[280px] flex-col items-center">
-        <AvatarBox />
         <div className="mb-4 flex flex-col items-center">
-          <Heading>{t("login.title")}</Heading>
+          <Heading>Bienvenido a Ekomerce Admin</Heading>
           <Text size="small" className="text-ui-fg-subtle text-center">
             {t("login.hint")}
           </Text>
